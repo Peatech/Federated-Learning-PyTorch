8d2e0963df0328d90ed33b01fd02a84a2c4a2fb7
Peatech <90410753+Peatech@users.noreply.github.com>
2024-10-29 14:36:44 +0000

    Update federated_main.py

--- src/federated_main.py
+++ src/federated_main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     args = args_parser()
     exp_details(args)
 
-    if args.gpu_id:
+    if args.gpu:
         torch.cuda.set_device(args.gpu_id)
     device = 'cuda' if args.gpu else 'cpu'
 
